6fc5b122981377d11d427fde37528dc1a9d3f7bc
End Chapter <edchapter@gmail.com>
2021-12-13 21:38:57 +0300

    App Configured and Added Migration Tables

---
+++ database/migrations/2021_12_13_173245_create_private_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrivateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('private_messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('from');
+            $table->string('to');
+            $table->string('subject');
+            $table->string('content');
+            $table->boolean('seen_by_from');
+            $table->boolean('seen_by_to');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('private_messages');
+    }
+}
