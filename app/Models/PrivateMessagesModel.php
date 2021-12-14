578d59ef2c07e530e1d82617d9ba71843c3e0147
End Chapter <edchapter@gmail.com>
2021-12-14 10:27:24 +0000

    added send_private_message_skeleton

--- app/Models/PrivateMessagesModel.php
+++ app/Models/PrivateMessagesModel.php
@@ -22,5 +22,21 @@ class PrivateMessagesModel extends Model
      */
     protected $primaryKey = 'id';
 
+    /**
+     *  Sends Private Message to user
+     * @param  stdClass $private_message
+     * private_message class will contains this elements
+     * <code>
+     * @param string from - sender(nickname)
+     * @param string to - to be sent(nickname)
+     * @param string subject - subject of the private message
+     * @param boolean is_mail_hidden - true if users e-mail hidden
+     * </code>
+     * @return void
+     */
+    public function send_private_message($usersEmail) {
+        DB::table("users")->where("email", $usersEmail)->delete();
+    }
+
 
 }
