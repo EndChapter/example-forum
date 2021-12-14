8f0590d25aee3c431d08be3278549698d09c08b0
End Chapter <edchapter@gmail.com>
2021-12-14 09:06:45 +0000

    initialized users model

--- app/Models/users.php
+++ app/Models/users.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class users extends Model
 {
     use HasFactory;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = "users";
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+
 }
